b06afb4396ffe529038f3b3336c071dc14ea5414
Student User <student@workstation.lab.example.com>
2024-08-16 08:45:28 -0400

    AD141 Exercises

--- m/AD141-apps/collections/examples/adding_to_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-fruits = {"a": "Apples", "b": "Bananas"}
-print(fruits)
-fruits["s"] = "Strawberries"    # Add a new key-value pair
-print(fruits)
--- m/AD141-apps/collections/examples/unpacking_lists.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-days = ["Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday"]
-mon, tue, wed, thu, fri = days
-print(mon, fri)
